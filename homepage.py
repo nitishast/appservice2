2aeeeda3eb9cf1f786bb18f661905a0522f1faf1
nitishast <nitishast@gmail.com>
2024-10-01 15:37:30 +0530

    starter

---
+++ homepage.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="���",
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+# st.sidebar.success("Select a demo above.")
